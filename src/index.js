b418ef721832b015c419bf85add2ce410d20991f
Laura Castro <lauracastrotech@gmail.com>
2025-05-20 12:51:18 -0400

    converts doubleTheNums to arrow function

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
-const doubleTheNums = function(arr) {
+// const doubleTheNums = function(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     arr[i] *= 2;
+//   }
+//   return arr;
+// };
+
+// convert above function to an arrow function
+const doubleTheNums = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     arr[i] *= 2;
   }
   return arr;
 };
 
-// convert above function to an arrow function
-
+console.log(doubleTheNums([2,4,5]));
 
 
 // Write a function that prints out "Good morning, class!"
+const goodMorning = () =>{
+  console.log('Good morning class!');
+};
 
+goodMorning();
 
 
 // Write a function that prints a random number (integer) between 1 and 100.
